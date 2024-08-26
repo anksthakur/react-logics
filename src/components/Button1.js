b95c9765eee426d61cb959d5f45da81dd70ffcb4
anup thakur <anksthakur7@gmail.com>
2024-08-26 12:17:31 +0530

    function changed

--- src/components/Button1.js
+++ src/components/Button1.js
@@ -14,6 +14,8 @@ const Button1 = () => {
   const url0 = "http://localhost:5000/0";
   const url1 = "http://localhost:5000/1";
   const url2 = "http://localhost:5000/2";
+  let button1Time = 1000;
+  let button2Time = 5000;
 
   const setToggle = () => {
     setOn(!on);
@@ -28,55 +30,35 @@ const Button1 = () => {
     return newController.signal;
   }, [controller]);
 
-  const button1Data= useCallback(async () => {
-    setLoading(true);
-    const signal = cancelRequest();
-    setTimeout(async () => {
-      try {
-        const res = await axios.get(url0, { signal });
-        const data = res.data;
-        log("button 1 data :", data);
-        setData(data);
-      } catch (error) {
-        if (axios.isCancel(error)) {
-          log("Request canceled button 1:", error.message);
-        } else {
-          log("Error in button 1:", error);
-          setError(error.message);
+  const button1Data = useCallback(async (url, delay) => {
+      setLoading(true);
+      const signal = cancelRequest();
+      setTimeout(async () => {
+        try {
+          const res = await axios.get(url, { signal });
+          const data = res.data;
+          log("button 1 data :", data);
+          setData(data);
+        } catch (error) {
+          if (axios.isCancel(error)) {
+            log("Request canceled button 1:", error.message);
+          } else {
+            log("Error in button 1:", error);
+            setError(error.message);
+          }
+        } finally {
+          setLoading(false);
         }
-      } finally {
-        setLoading(false);
-      }
-    }, 1000);
-  }, [cancelRequest,log]);
-
-  const button2Data = useCallback(async () => {
-    setLoading(true)
-    const signal = cancelRequest();
-    setTimeout(async () => {
-      try {
-        const res = await axios.get(url1,{signal});
-        const data = res.data;
-        log("button 2 data :", data);
-        setData(data);
-      } catch (error) {
-        if (axios.isCancel(error)) {
-          log("Request canceled button 2:", error.message);
-        } else {
-          log("Error in button 2:", error);
-          setError("button error", error.message);
-        }
-      } finally {
-        setLoading(false);
-      }
-    }, 5000);
-  },[cancelRequest,log]);
+      }, delay);
+    },
+    [cancelRequest, log]
+  );
 
   const button3Data = useCallback(async () => {
-    setLoading(true)
-    const signal = cancelRequest(); 
+    setLoading(true);
+    const signal = cancelRequest();
     try {
-      const res = await axios.get(url2,{signal});
+      const res = await axios.get(url2, { signal });
       if (res.status === 200) {
         throw new Error("User Not Found");
       }
@@ -89,21 +71,23 @@ const Button1 = () => {
     } finally {
       setLoading(false);
     }
-  },[cancelRequest,log]);
+  }, [cancelRequest, log]);
 
   return (
     <>
       <div className="flex justify-between bg-blue-600 p-4 mx-2">
         <h1 className="text-white text-xl font-bold">Navbar</h1>
         <button onClick={setToggle} className="flex px-4 py-2 rounded overflow-hidden">
-          <span className={`flex-1 px-2 py-1 text-center ${on ? 'bg-white text-black' : 'bg-black text-white'}`}>{on ? "ON" : "OFF"}</span>
-          <span className={`flex-1 px-2 py-1 text-center ${on ? 'bg-black text-white' : 'bg-white text-black'}`}>{on ? "OFF" : "ON"}</span>
+          <span className={`flex-1 px-2 py-1 text-center ${   on ? "bg-white text-black" : "bg-black text-white" }`}
+          >{on ? "ON" : "OFF"}</span>
+          <span className={`flex-1 px-2 py-1 text-center ${   on ? "bg-black text-white" : "bg-white text-black" }`}>
+            {on ? "OFF" : "ON"}</span>
         </button>
       </div>
       <div className="flex mt-4 space-x-4 mx-2 justify-between">
-        <Buttons button="A" onClick={button1Data}/>
-        <Buttons button="B" onClick={button2Data}/>
-        <Buttons button="C" onClick={button3Data}/>
+        <Buttons button="A" onClick={() => button1Data(url0, button1Time)}/>
+        <Buttons button="B" onClick={() => button1Data(url1, button2Time)}/>
+        <Buttons button="C" onClick={button3Data} />
       </div>
       <div className="flex justify-center items-center mt-[100px]">
         <div className="loadingDiv bg-gray-100 w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4 p-4 shadow-lg border rounded-md">
@@ -121,7 +105,7 @@ const Button1 = () => {
             <div className="text-center text-red-500">{error}</div>
           ) : data ? (
             <div className="text-center">
-              <img src={data.img} alt="error.jpeg" className="w-72 h-40 mx-auto shadow-slate-700 border rounded-md" />
+              <img src={data.img} alt="error.jpeg" className="w-72 h-40 mx-auto shadow-slate-700 border rounded-md"/>
               <h1 className="mt-2 text-lg font-semibold">Name: {data.name}</h1>
               <h2 className="mt-2 text-md">Age: {data.age}</h2>
               <div className="flex justify-around mt-4">
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -4,8 +4,10 @@ const Buttons = ({ button, onClick }) => {
   return (
     <div>
       <button
-        className="bg-green-200 border border-black hover:bg-green-500 text-black rounded-md px-4 py-2 w-full" onClick={onClick}>{button}</button>
+        className="bg-green-200 border border-black hover:bg-green-500 text-black rounded-md px-4 py-2 w-full"
+        onClick={onClick}>{button}</button>
     </div>
   );
 };
+
 export default Buttons;
